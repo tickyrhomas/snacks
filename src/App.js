703a23630505bddd125d4b89227a5cd3e7ae71b0
Ricky Thomas <rick@tastemade.com>
2017-07-20 20:24:51 -0500

    installed styled

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
 import logo from './logo.svg';
 import './App.css';
+
+const Title = styled.h1`
+  font-size: 6.5em;
+  text-align: center;
+  color: white;
+`;
+
+const Container = styled.div`
+  background-color: gray;
+  display: grid;
+  height: 100vh;
+  grid-template-columns: auto 1fr;
+  grid-template-rows: auto auto 1fr;
+`;
+
+const SpinningLogo = styled.img`
+  animation: App-logo-spin infinite 20s linear;
+  height: 200px;
+`
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{backgroundColor:'gray'}}>
-        <p className="App-intro">
-          <img className="App-logo" src='andrew_snack.png'/>
+      <Container>
+        <Title>
+          <SpinningLogo src='andrew_snack.png'/>
           Coming Soon!
-          <img className="App-logo" src='ricky_snack.png'/>
-        </p>
-      </div>
+          <SpinningLogo src='ricky_snack.png'/>
+        </Title>
+      </Container>
     );
   }
 }
