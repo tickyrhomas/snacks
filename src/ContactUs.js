8960e52d625af646bc706ca78f1fa4546b132088
Ricky Thomas <rick@tastemade.com>
2017-08-05 15:54:24 -0500

    got react-router working

---
+++ src/ContactUs.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+export class ContactUs extends Component {
+  render () {
+    return (
+      <Container>
+        <p>ContactUs Page</p>
+      </Container>
+    )
+  }
+}
+
+const Container = styled.div`
+  grid-area: main;
+  display: grid;
+  grid-template-columns: 22% 22% 22% 22%;
+  grid-template-rows: 1fr 1fr;
+  grid-column-gap: 3%;
+  background-color: #E6E6E6;
+`
---
+++ src/OtherProjects.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export class OtherProjects extends Component {
+  render () {
+    return <p>Other Projects Page</p>
+  }
+}
